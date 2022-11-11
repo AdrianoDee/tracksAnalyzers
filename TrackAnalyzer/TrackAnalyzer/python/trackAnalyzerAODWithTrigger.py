3a3eb6460ccc619b52726b929a34335d5cb96505
AdrianoDee <adriano.diflorio@ba.infn.it>
2022-11-12 00:34:31 +0100

    adding HLT objects config - clean

--- TrackAnalyzer/TrackAnalyzer/python/trackAnalyzerAODWithTrigger.py
+++ TrackAnalyzer/TrackAnalyzer/python/trackAnalyzerAODWithTrigger.py
@@ -51,42 +51,7 @@ process.source = cms.Source("PoolSource",
     #eventsToProcess = cms.untracked.VEventRange(eventsToProcess),
 )
 
-from PhysicsTools.PatAlgos.tools.trackTools import makeTrackCandidates
-makeTrackCandidates(process,
-    label        = 'TrackCands',                  # output collection
-    tracks       = cms.InputTag('generalTracks'), # input track collection
-    particleType = 'pi+',                         # particle type (for assigning a mass)
-    preselection = 'pt > 0.7',                    # preselection cut on candidates
-    selection    = 'pt > 0.7',                    # selection on PAT Layer 1 objects
-    isolation    = {},                            # isolations to use (set to {} for None)
-    isoDeposits  = [],
-    mcAs         = None                           # replicate MC match as the one used for Muons
-)
-process.patTrackCands.embedTrack = True
-process.patTrackCands.src = cms.InputTag("patAODTrackCandsUnfiltered")
-
-process.trackHLTMatcher = cms.EDProducer(
-  # matching in DeltaR, sorting by best DeltaR
-  "PATTriggerMatcherDRLessByR"
-  # matcher input collections
-, src     = cms.InputTag( 'patTrackCands' )
-, matched = cms.InputTag( 'patTrigger' )
-  # selections of trigger objects
-, matchedCuts = cms.string( 'path( "HLT_DoubleMu4_JpsiTrk_Displaced_v*", 1, 0 )' ) # input does not yet have the 'saveTags' parameter in HLT
-  # selection of matches
-, maxDPtRel   = cms.double( 0.5 ) # no effect here
-, maxDeltaR   = cms.double( 0.5 )
-, maxDeltaEta = cms.double( 0.2 ) # no effect here
-  # definition of matcher output
-, resolveAmbiguities    = cms.bool( True )
-, resolveByMatchQuality = cms.bool( True )
-)
-
-
-
 from PhysicsTools.PatAlgos.tools.trigTools import *
-switchOnTrigger( process, None, None, None, None, '' )
-switchOnTriggerMatching( process, triggerMatchers = [ 'trackHLTMatcher' ], outputModule = '' )
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(par.e))
 
